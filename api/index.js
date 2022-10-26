98c67aa48a63172cccb222616e7ab0ca9470ac2d
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-25 21:41:21 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { port } = require('./src/utils/config/index');
 
+server.set("port", process.env.PORT || 3001);
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    console.log('Server listening at 3001'); // eslint-disable-line no-console
+    console.log(`Server listening at ${app.get("port")}`); // eslint-disable-line no-console
   });
 });
